83659ab22d9c8876dd2c0ededf205744ce1e5384
Mayowa J Michael <91376779+Holymikel@users.noreply.github.com>
2022-10-26 22:44:27 +0100

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+ class Song(models.Model):
+    title = models.CharField(max_length=30)
+    release_date = models.DateField()
+    likes = models.CharField(max_length=30)
+    artiste_id = models.CharField(max_length=30
+   
+class Lyric(models.Model):
+    content = models.CharField(max_length=30)
+    song_id = models.CharField(max_length=30)
+
