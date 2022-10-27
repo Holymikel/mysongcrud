d4ebcd2d54427cdb86306e96a9d7401dfffad046
Mayowa J Michael <91376779+Holymikel@users.noreply.github.com>
2022-10-27 21:42:54 +0100

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -5,7 +5,7 @@ class Artiste(models.Model):
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
 
- class Song(models.Model):
+class Song(models.Model):
     title = models.CharField(max_length=30)
     release_date = models.DateField()
     likes = models.CharField(max_length=30)
